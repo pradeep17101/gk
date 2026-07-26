9c316724f245c7a9e99bd66db8e303eccd42283d
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 01:54:18 +0530

    Add identifier validation program with examples of valid and invalid Python identifiers

---
+++ identifier_validation.py
@@ -0,0 +1,19 @@
+#Name: T.Pradeep
+#Lab:01
+#Task:02
+#Program:Valid and Invalid Python Identifiers
+#2value=33
+value_2=40
+_hidden=21
+my_class='D section'
+my_var="welcome"
+MyClass='Pradeep'
+total_amount=7000.00
+#Here 2value is an invalid variable because its sarting with digit
+#value_2 is valid variable beacause its following identifier naming rules
+#_hidden is valid variable
+#class is an invalid variable because its a keyword
+#may-var is invalid variable because it contains a hyphen
+#MyClass is valid variable
+# total$ is inavlid variable because it contains '$' symbol
+
