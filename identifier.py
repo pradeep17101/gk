1f7a5dcac3d7143209e088e4e7417e75b01e5d83
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 01:38:12 +0530

    Add initial implementation of identifier module with greeting function and student class

---
+++ identifier.py
@@ -0,0 +1,12 @@
+number = 10
+MAX_VALUE = 100
+def greet():
+    print("Function Name: greet")
+class Student:
+    pass
+user_name = "Pradeep"
+print("Variable:", number)
+print("Constant-style Name:", MAX_VALUE)
+print("Class Name:", Student.__name__)
+print("Underscore Name:", user_name)
+greet()
