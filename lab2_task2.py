82cdd21d63a783a46d60a1950e8f325cd137d7f0
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 11:36:26 +0530

    Add keyword checker program in Python

---
+++ lab2_task2.py
@@ -0,0 +1,13 @@
+#Name: T.Pradeep
+#Lab:02
+#Task:02
+#Program:keyword checker in python
+import keyword
+print(keyword.iskeyword("if"))
+print(keyword.iskeyword("False"))
+print(keyword.iskeyword("async"))
+print(keyword.iskeyword("name"))
+print(keyword.iskeyword("student"))
+print(keyword.iskeyword("def"))
+print(keyword.iskeyword("or"))
+
