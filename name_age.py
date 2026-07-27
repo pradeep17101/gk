bef08f6a7d7fc326401deac66e63e03a3cbe46f0
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 17:07:36 +0530

    Add name_age.py program for age calculation based on user input

---
+++ name_age.py
@@ -0,0 +1,8 @@
+#Name: T.Pradeep
+#Lab:05
+#Task:01
+#Program:Age Calculator for further year
+name=input("Whats your name?")
+age=input("Whats your age ?")
+print("Hello",name,", you will turn",int(age)+1,"next year")
+
