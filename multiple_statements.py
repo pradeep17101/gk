5a514a24b4a30e13a6c23916752d75987625d65d
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 12:58:12 +0530

    Add multiple_statements.py program demonstrating multiple statements in a single line

---
+++ multiple_statements.py
@@ -0,0 +1,6 @@
+#Name: T.Pradeep
+#Lab:04
+#Task:03
+#Program:multiple statements in single line
+a=10;b=15
+print("I love playing games");name=input("What's your name?");print("i think u are ",name)
