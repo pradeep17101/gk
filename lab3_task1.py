0352e46a74bad6184245e25100b5fc57fa0b0377
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 12:05:35 +0530

    Add initial implementation of variables and data types program

---
+++ lab3_task1.py
@@ -0,0 +1,12 @@
+#Name: T.Pradeep
+#Lab:03
+#Task:01
+#Program:Variables and Data Types
+name="T Pradeep" 
+age=19
+height=5.9
+is_student=True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
