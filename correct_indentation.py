07628ff2b3b3e0490d52532985cedd1b695c68bc
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 18:09:31 +0530

    correct indentation program to check if a number is positive or negative

---
+++ correct_indentation.py
@@ -0,0 +1,9 @@
+#name=T.Pradeep
+#lab=06
+#task=03
+#program=change the pseudo code to python code to check whether the number is positive or negative
+x = int(input("Enter a number: "))
+if x > 0:
+    print("Positive")
+else:
+    print("Non-positive")
