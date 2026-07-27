0aa1768251284dcbf597f1606c01d3bd6b1f93d6
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 13:01:08 +0530

    Add challenge_4.py program demonstrating basic statements and expressions in Python

---
+++ challeng_4.py
@@ -0,0 +1,9 @@
+#Name: T.Pradeep
+#Lab:04
+#Task:Challenge 4
+#A Statement is an instruction for python execution
+x=40
+#An expression is a combination of values,variables, and operators that produces a value
+y=x+7
+print(x)#statement
+print(y)#statement 
