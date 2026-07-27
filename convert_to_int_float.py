097a80b91297256fc305cba8e170697b97f912ef
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 17:16:05 +0530

    Add convert_to_int_float.py program for string to int and float conversion with basic operations

---
+++ convert_to_int_float.py
@@ -0,0 +1,17 @@
+#name=T.Pradeep
+#lab=05
+#task=02
+#program= write a program to convert string to int and float and perform operations
+
+# Simple Calculator Program
+# Input two numbers as strings
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+# Convert strings to float
+num1 = float(num1)
+num2 = float(num2)
+# Perform operations
+print("Sum =", num1 + num2)
+print("Difference =", num1 - num2)
+print("Product =", num1 * num2)
+print("Quotient =", num1 / num2)
