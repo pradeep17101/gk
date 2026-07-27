5fc9cfe9b32de63109e5617eaf9ff8cbe340406e
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 18:27:28 +0530

    Add scriptname_arguments.py to demonstrate usage of script name and command line arguments

---
+++ scriptname_arguments.py
@@ -0,0 +1,13 @@
+#name=T.Pradeep
+#lab=07 
+#task=03
+#program=script name and arguments
+# script.py
+
+import sys
+
+# Print script name
+print("Script Name:", sys.argv[0])
+
+# Print total number of arguments
+print("Total Arguments:", len(sys.argv))
