09243a88e5f8883fece56e353a6ddec8608619e3
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 18:13:47 +0530

    program to demonstrate three levels of nested indentation

---
+++ challenge_6.py
@@ -0,0 +1,12 @@
+#name=T.Pradeep
+#lab=06
+#task=challenge 6
+#program=Three levels of nested indentation
+# Three levels of nested indentation
+# for -> if -> for
+
+for i in range(1, 6):
+    if i > 0:
+        for j in range(i):
+            print("*", end="")
+    print()
