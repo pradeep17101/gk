7089c91977491e15aae550101a4784b5524748cc
T PRADEEP <126693884+pradeep17101@users.noreply.github.com>
2026-07-27 22:34:53 +0530

    Add output comments for clarity in identifier.py
    Added output comments to clarify printed values.

--- identifier.py
+++ identifier.py
@@ -10,3 +10,11 @@ print("Constant-style Name:", MAX_VALUE)
 print("Class Name:", Student.__name__)
 print("Underscore Name:", user_name)
 greet()
+
+
+#Output:
+#Variable: 10
+#Constant-style Name: 100
+#Class Name: Student
+#Underscore Name: Pradeep
+#Function Name: greet
