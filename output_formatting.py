b3e878ddb076ba4537705dee6f143bfcfd77d11d
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 17:18:44 +0530

    Add output_formatting.py program demonstrating output formatting methods in Python

---
+++ output_formatting.py
@@ -0,0 +1,11 @@
+#Name: T.Pradeep
+#Lab:05
+#Task:03
+#Program:Output Formatting Methods
+name="Pradeep" 
+age=19
+print("Name:",name,",Age:",age)
+#str.format()
+print("Name :{} ,Age : {}".format(name,age))
+#f-string
+print(f"Name:{name}, Age:{age}")
