582e6108e69163f4dd23e312b802125bb9356d86
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 11:42:46 +0530

    for=5        ^ SyntaxError: invalid syntax

---
+++ lab2_task3.py
@@ -0,0 +1,13 @@
+#Name: T.Pradeep
+#Lab:02
+#Task:03
+#Program:create a variable name
+for=5
+True=10
+#Errors are shown below
+#for=5
+#   ^
+#SyntaxError: invalid syntax
+#True=10
+#    ^
+#SyntaxError: cannot assign to True
