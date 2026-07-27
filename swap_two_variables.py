bb53e7cefc8bd33b7b57b8cefc8e1d5d67003b0f
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 12:22:27 +0530

    Add swap_two_variables.py program demonstrating variable swapping techniques

---
+++ swap_two_variables.py
@@ -0,0 +1,21 @@
+#Name: T.Pradeep
+#Lab:03
+#Task:03
+#Program:Swapping of two variables
+#creating temp variable
+a=input("Enter 'a'value:")
+b=input("Enter 'b' value:")
+temp=a
+a=b
+b=temp
+print("After swapping")
+print("a =",a)
+print("b =",b)
+#Swapping by tuple unpacking
+a=input("Enter 'a'value:")
+b=input("Enter 'b' value:")
+a,b=b,a
+print("After swapping by tuple unpacking i.e a,b=b,a")
+print("a =",a)
+print("b =",b)
+
