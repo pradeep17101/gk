7e130af3c7704104d5e579bd05f50d12d8e97b5b
T PRADEEP <126693884+pradeep17101@users.noreply.github.com>
2026-07-27 22:46:28 +0530

    Refactor multiple statements into single line

--- multiple_statements.py
+++ multiple_statements.py
@@ -4,3 +4,10 @@
 #Program:multiple statements in single line
 a=10;b=15
 print("I love playing games");name=input("What's your name?");print("i think u are ",name)
+
+
+#Output:
+#I love playing games
+#What's your name?pradeep
+#i think u are  pradeep
+
