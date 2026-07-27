708e5519e3f9e96ca8f922d1773e1a0bd7fb5b93
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 11:31:18 +0530

    Add program to list Python keywords and soft keywords

---
+++ lab2_task1.py
@@ -0,0 +1,9 @@
+#Name: T.Pradeep
+#Lab:02
+#Task:01
+#Program:List of python keywords
+import keyword
+print(keyword.kwlist)
+print("Total number of keywords:",len(keyword.kwlist))
+print("Soft keywors:",keyword.softkwlist)
+print("Total no of Soft Keywords:",len(keyword.softkwlist))
