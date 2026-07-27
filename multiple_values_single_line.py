41be2e5acedb729f1a5430253b66a1fd6aace9e7
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 17:34:28 +0530

    Add multiple_values_single_line.py program to input multiple values and calculate their sum

---
+++ multiple_values_single_line.py
@@ -0,0 +1,9 @@
+#name=T.Pradeep
+#lab=05
+#task=04
+#program=Write a program to input multiple values in single line and find sum of them
+# Input numbers in one line
+numbers = input("Enter numbers separated by spaces: ")
+# Convert to integers and find sum
+numbers = list(map(int, numbers.split()))
+print("Sum =", sum(numbers))
