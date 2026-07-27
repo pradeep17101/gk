3801732a4e892ef076d8a1ddae45bccf12df3ed9
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 12:32:07 +0530

    Add challenge_3.py program to calculate the area and circumference of a circle

---
+++ challenge_3.py
@@ -0,0 +1,10 @@
+#Name: T.Pradeep
+#Lab:03
+#Task:challenge 3
+#Program:Calculatng the area and circumfernce of a circle
+radius=float(input("Enter the radius\n"))
+area=(3.14*radius*radius)
+circumference=(2*3.14*radius)
+print("Area of circle is",area)
+print("Circumference of circle is",circumference)
+
