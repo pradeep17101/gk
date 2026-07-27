187d896970d19e14a61b26f576876da43f48bb6c
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 12:49:53 +0530

    Add statement_types.py program demonstrating assignment, loop, conditional, and print statements in Python

---
+++ statement_types.py
@@ -0,0 +1,18 @@
+#name=T.pradeep
+#lab=4
+#task=1
+# 1. ASSIGNMENT STATEMENT
+# Storing a list of numbers in a variable
+numbers = [1, 2, 3, 4, 5, 6]
+
+# 2. LOOP STATEMENT
+# Iterating through each number in the list
+for number in numbers:
+
+# 3. CONDITIONAL IF STATEMENT
+    # Checking if the current number is even
+    if number % 2 == 0:
+
+# 4. PRINT STATEMENT
+        # Displaying the even number to the screen
+        print(f"{number} is an even number.")
