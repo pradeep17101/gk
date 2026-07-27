1004da563d38f2ea0c151b90578d407328469062
T PRADEEP <ptalabhaktula@gmail.com>
2026-07-27 18:33:19 +0530

    Add challenge_7.py to accept two numbers as command line arguments and print their sum

---
+++ challenge_7.py
@@ -0,0 +1,14 @@
+#name=T.Pradeep
+#lab=07 
+#task=challenge 7
+#program=Write a program to accept two numbers as command line arguments and print their sum
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--num1", type=int, default=0)
+parser.add_argument("--num2", type=int, default=0)
+
+args = parser.parse_args()
+
+print("Sum =", args.num1 + args.num2)
